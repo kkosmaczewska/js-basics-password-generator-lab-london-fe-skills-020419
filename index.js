6d43eecda0b84c36377441242bb856fa827489c1
Kasia Kosmaczewska <kakosmaczewska@gmail.com>
2019-04-01 17:20:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 window.addEventListener("load", () => {
   //start your code here
-});
+  document.getElementById("myForm").addEventListener("submit", submit);
+  });
+  
+    
+  function generatePassword(passwordLength) {
+  const lowerChars = "abcdefghijklmnopqrstuvwxyz";
+  const upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numberChars = "0123456789";
+  const specialChars = "#$%&()*+,-./:;<=>?@[\]^_{|}~";
+  let allChars = lowerChars + upperChars + numberChars + specialChars;
+  for(var i=0; i < passwordLength; i++ );
+  let password = allChars.charAt(Math.floor(Math.random() * allChars.length));
+  return password;
+    }
+  
+  function submit(e){
+    e.preventDefault();
+    console.log();
+    let passwordLength = document.getElementById("passwordLength").value;
+    console.log(passwordLength);
+    let password = generatePassword(passwordLength);
+    document.getElementById("result").innerHTML = password;
+    }
+  
+  
